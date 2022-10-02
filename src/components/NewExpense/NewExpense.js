fb7b03acd22b5688b2af055a7ecbd7e889983297
Anton V <vinnichenkoanton.cv@gmail.com>
2022-10-02 14:15:39 +0300

    uuid package was added and used to create ids for ExpenseData

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,35 +1,42 @@
-import { useState } from 'react';
-import './NewExpense.css';
-import './ExpenseForm';
-import ExpenseForm from './ExpenseForm';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./NewExpense.css";
+import "./ExpenseForm";
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        props.onAddExpense(expenseData);
-        setIsEditing(false);
-
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: uuidv4(),
     };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
 
-    const startEditingHandler = () => {
-        setIsEditing(true);
-    }
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
 
-    const stopEditingHandler = ()=> {
-        setIsEditing(false);
-    };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
 
-    return (
-        <div className="new-expense">
-            {!isEditing && <button onClick={startEditingHandler}>Add new Expense</button>}
-            {isEditing && <ExpenseForm onCancel = {stopEditingHandler} onSaveExpenseData={saveExpenseDataHandler} />}
-        </div>
-    );
-}
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onCancel={stopEditingHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
